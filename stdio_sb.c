70e76fdc209c6def806b18284d74be032a5dd885
Wictor Lund <wlund@abo.fi>
2022-12-21 11:18:04 +0200

    stdio_sb.c: dump my current changes

--- stdio_sb.c
+++ stdio_sb.c
@@ -20,32 +20,102 @@
 
 #include <sys/mount.h>
 
-#include <stdio.h>
+#include <ctype.h>
 #include <err.h>
-#include <unistd.h>
-#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
 
 #include <sched.h>
 
 #define INITIAL_BUF_SZ ((size_t)1024)
 
-const static char *prog_name;
+#define DEFAULT_UNSHARE_FLAGS                                                  \
+    ((int)(CLONE_FS | CLONE_NEWNS | CLONE_NEWIPC | CLONE_NEWNET))
 
-static void usage()
+#define BIND_MOUNT_FREE_NONE (0x00)
+#define BIND_MOUNT_FREE_SRC (0x01)
+#define BIND_MOUNT_FREE_TARGET (0x02)
+#define BIND_MOUNT_FREE_BOTH (BIND_MOUNT_FREE_SRC | BIND_MOUNT_FREE_TARGET)
+
+#define USAGE_PRINT_EXTENDED (-1)
+#define USAGE_PRINT_BASIC (0)
+
+#define PROG_EXIT_STATUS_ERR (1)
+#define PROG_EXIT_STATUS_OK (0)
+
+static const char *prog_name;
+
+static const char *
+prog_name_last_component()
 {
-    errx(1,
-         "usage: %s PATH_CHROOT PATH_DATA PATH_MOUNTPOINT =PATH_OTHERS... "
-         "PATH_PROG PROG_ARGS...",
-         prog_name);
+    const char *ret;
+
+    ret = rindex(prog_name, '/');
+
+    return ret == NULL ? prog_name : ret + 1;
 }
 
-static void check_and_fixup_arg_path(const char *arg_name, char *path)
+noreturn static void
+usage(int print_mode, int exit_status, const char *msg, ...)
+{
+    const char *prog_name_;
+    va_list ap;
+    int i;
+    static const char *flag_doc[] = {
+        "-F,-f",
+        "Disable/enable CLONE_FS flag to unshare()",
+        "-C,-c",
+        "Disable/enable CLONE_CGROUP flag to unshare()",
+        "-I,-i",
+        "Disable/enable CLONE_NEWIPC flag to unshare()",
+        "-N,-n",
+        "Disable/enable CLONE_NEWNET flag to unshare()",
+        "-U,-u",
+        "Disable/enable CLONE_NEWUSER flag to unshare()",
+        "-T,-t",
+        "Disable/enable CLONE_NEWUTS flag to unshare(), i.e. \"hostname\" "
+        "namespace",
+        "-V,-v",
+        "Disable/enable CLONE_SYSVSEM flag to unshare()",
+        NULL};
+    prog_name_ = prog_name_last_component();
+    if (msg != NULL) {
+        fprintf(stderr, "%s: %s", prog_name_,
+                exit_status == PROG_EXIT_STATUS_OK ? "" : "error: ");
+        va_start(ap, msg);
+        vfprintf(stderr, msg, ap);
+        va_end(ap);
+        fprintf(stderr, "\n");
+    }
+    fprintf(stderr,
+            "%s: usage: %s [-FfCcIiNnUuTtVvdh] PATH_CHROOT PATH_DATA "
+            "PATH_MOUNTPOINT =PATH_OTHERS... PATH_PROG PROG_ARGS...\n",
+            prog_name_, prog_name);
+    if (print_mode == USAGE_PRINT_EXTENDED) {
+        i = 0;
+        while (flag_doc[i] != NULL) {
+            fprintf(stderr, "\t%s\t%s\n", flag_doc[i], flag_doc[i + 1]);
+            i += 2;
+        }
+        fprintf(stderr, "\t-d\tPrint debug messages\n");
+        fprintf(stderr, "\t-h\tShow this message and exit\n");
+    }
+    exit(exit_status);
+}
+
+static void
+check_and_fixup_arg_path(const char *arg_name, char *path)
 {
     int path_len;
 
     if (path == NULL)
-        usage();
+        usage(USAGE_PRINT_BASIC, PROG_EXIT_STATUS_ERR, NULL);
 
     if (*path != '/')
         errx(1, "%s does not start with '/'", arg_name);
@@ -61,19 +131,115 @@ static void check_and_fixup_arg_path(const char *arg_name, char *path)
     return;
 }
 
-int main(int argc, char **argv)
+static int
+concatenate_paths_into_buf(char **buf_p, size_t *buf_sz_p, const char *path1,
+                           const char *path2)
 {
-    int ret, i;
+    int buf_len;
+    char *buf;
+
+    while (1) {
+        buf_len = snprintf(*buf_p, *buf_sz_p, "%s%s", path1, path2);
+        if (buf_len < *buf_sz_p)
+            return buf_len < 0 ? -1 : buf_len;
+        *buf_p = realloc(buf = *buf_p, buf_len);
+        if (*buf_p == NULL) {
+            *buf_p = buf;
+            return -1;
+        } else
+            *buf_sz_p = buf_len;
+    }
+}
+
+static void
+bind_mount(char *path_src, char *path_target, int free_paths)
+{
+    int ret;
+
+    ret = mount(path_src, path_target, NULL, MS_BIND, NULL);
+
+    if (ret == 0)
+        return;
+
+    fprintf(stderr, "%s: mount() bind %s to %s: %s\n",
+            prog_name_last_component(), path_src, path_target, strerror(errno));
+
+    if (free_paths & BIND_MOUNT_FREE_SRC)
+        free(path_src);
+
+    if (free_paths & BIND_MOUNT_FREE_TARGET)
+        free(path_target);
+
+    exit(1);
+}
+
+int
+main(int argc, char **argv)
+{
+    int i, ch;
     static char *path_chroot, *path_data, *path_mountpoint, *path_prog,
         *execve_envp[1], **argv_others, **argv_execve, *buf, *buf_;
     static size_t buf_len, buf_sz;
 
+    static int unshare_flags = DEFAULT_UNSHARE_FLAGS, debug_flag = 0;
+
     prog_name = argv[0];
 
-    if (argc < 5)
-        usage();
+    while ((ch = getopt(argc, argv, "FfCcIiNnUuTtVvdh")) != -1) {
+        switch (ch) {
+        case 'F':
+        case 'f':
+            i = CLONE_FS;
+            break;
+        case 'C':
+        case 'c':
+            i = CLONE_NEWCGROUP;
+            break;
+        case 'I':
+        case 'i':
+            i = CLONE_NEWIPC;
+            break;
+        case 'N':
+        case 'n':
+            i = CLONE_NEWNET;
+            break;
+        case 'U':
+        case 'u':
+            i = CLONE_NEWUSER;
+            break;
+        case 'T':
+        case 't':
+            i = CLONE_NEWUTS;
+            break;
+        case 'V':
+        case 'v':
+            i = CLONE_SYSVSEM;
+            break;
+        case 'h':
+            usage(USAGE_PRINT_EXTENDED, PROG_EXIT_STATUS_OK, NULL);
+            continue;
+        case 'd':
+            debug_flag = 1;
+            continue;
+        default:
+            usage(USAGE_PRINT_BASIC, PROG_EXIT_STATUS_ERR,
+                  "invalid command-line flag");
+            break;
+        }
+        if (isupper(ch))
+            unshare_flags &= ~i;
+        else if (islower(ch))
+            unshare_flags |= i;
+    }
 
-    check_and_fixup_arg_path("PATH_CHROOT", path_chroot = argv[i = 1]);
+    argc -= optind;
+    argv += optind;
+
+    if (argc < 5)
+        usage(USAGE_PRINT_BASIC, PROG_EXIT_STATUS_ERR,
+              "too few command-line arguments, is %i, should be 5", argc);
+
+    check_and_fixup_arg_path("PATH_CHROOT", path_chroot = argv[i = 0]);
     check_and_fixup_arg_path("PATH_DATA", path_data = argv[++i]);
     check_and_fixup_arg_path("PATH_MOUNTPOINT", path_mountpoint = argv[++i]);
 
@@ -85,69 +251,77 @@ int main(int argc, char **argv)
     check_and_fixup_arg_path("PATH_PROG", path_prog = argv[i]);
     argv_execve = argv + i;
 
-    ret = unshare(CLONE_FS | CLONE_NEWNS | CLONE_NEWIPC);
-    if (ret == -1)
-        err(1, "unshare()");
+    if (debug_flag) {
+        fprintf(stderr, "%s DEBUG: running unshare() with flags: ", prog_name);
+        if (unshare_flags & CLONE_FS)
+            fprintf(stderr, "CLONE_FS ");
+        if (unshare_flags & CLONE_NEWCGROUP)
+            fprintf(stderr, "CLONE_NEWCGROUP ");
+        if (unshare_flags & CLONE_NEWIPC)
+            fprintf(stderr, "CLONE_NEWIPC ");
+        if (unshare_flags & CLONE_NEWNET)
+            fprintf(stderr, "CLONE_NEWNET ");
+        if (unshare_flags & CLONE_NEWNS)
+            fprintf(stderr, "CLONE_NEWNS ");
+        if (unshare_flags & CLONE_NEWUSER)
+            fprintf(stderr, "CLONE_NEWUSER ");
+        if (unshare_flags & CLONE_NEWUTS)
+            fprintf(stderr, "CLONE_NEWUTS ");
+        if (unshare_flags & CLONE_SYSVSEM)
+            fprintf(stderr, "CLONE_SYSVSEM ");
+        fprintf(stderr, "\n");
+    }
+
+    // TODO: add commandline parameters to configure which namespace to
+    // unshare()
+    // TODO: also unshare(): CLONE_NEWNET, CLONE_NEWPID (requires forking to
+    // work properly)
+    // TODO: maybe also unshare(): CLONE_NEWUSER, CLONE_NEWUTS, CLONE_SYSVSEM
+    // TODO: should we use clone() instead of unshare in case of CLONE_NEWPID?
+    // TODO: check whether the namespaces are destroyed when the (last)
+    //       process exists, ...  or if they are left dangling.
+    if (unshare(unshare_flags) == -1)
+        err(1, "unshare(), flags = %i", unshare_flags);
+
+    bind_mount(path_chroot, path_chroot, BIND_MOUNT_FREE_NONE);
+
+    if (mount("none", path_chroot, NULL, MS_PRIVATE, NULL) == -1)
+        err(1, "mount() make private %s", path_chroot);
 
     buf = malloc(buf_sz = INITIAL_BUF_SZ);
     if (buf == NULL)
         err(1, "malloc()");
 
-    while (1) {
-        buf_len = snprintf(buf, buf_sz, "%s%s", path_chroot, path_mountpoint);
-        if (buf_len < buf_sz)
-            break;
-        buf = realloc(buf_ = buf, buf_len);
-        if (buf == NULL) {
-            free(buf_);
-            err(1, "realloc()");
-        }
-    }
-
-    ret = mount(path_data, buf, NULL, MS_BIND, NULL);
-
-    if (ret == -1) {
+    if (concatenate_paths_into_buf(&buf, &buf_sz, path_chroot,
+                                   path_mountpoint) == -1) {
         free(buf);
-        err(1, "mount()");
+        err(1, "concatenate_paths_into_buf()");
     }
+
+    bind_mount(path_data, buf, BIND_MOUNT_FREE_TARGET);
 
     for (i = 0; argv_others[i] != NULL && *argv_others[i] == '='; i++) {
-        while (1) {
-            buf_len =
-                snprintf(buf, buf_sz, "%s%s", path_chroot, argv_others[i] + 1);
-            if (buf_len < buf_sz)
-                break;
-            buf = realloc(buf_ = buf, buf_len);
-            if (buf == NULL) {
-                free(buf_);
-                err(1, "realloc()");
-            }
-        }
-
-        ret = mount(argv_others[i] + 1, buf, NULL, MS_BIND, NULL);
-
-        if (ret == -1) {
+        if (concatenate_paths_into_buf(&buf, &buf_sz, path_chroot,
+                                       argv_others[i] + 1) == -1) {
             free(buf);
-            err(1, "mount()");
+            err(1, "concatenate_paths_into_buf()");
         }
+
+        bind_mount(argv_others[i] + 1, buf, BIND_MOUNT_FREE_TARGET);
     }
 
     free(buf);
 
-    ret = chroot(path_chroot);
-    if (ret == -1)
-        err(1, "chroot()");
+    if (chroot(path_chroot) == -1)
+        err(1, "chroot() to %s", path_chroot);
 
-    ret = chdir(path_mountpoint);
-    if (ret == -1)
-        err(1, "chdir() -> \"%s\"", path_mountpoint);
+    if (chdir(path_mountpoint) == -1)
+        err(1, "chdir() to %s", path_mountpoint);
 
     execve_envp[0] = NULL;
 
-    ret = execve(path_prog, argv_execve, execve_envp);
-
-    if (ret == -1)
-        err(1, "execve()");
+    if (execve(path_prog, argv_execve, execve_envp) == -1)
+        err(1, "execve() in chroot %s of %s", path_prog, path_chroot);
 
     return 0;
 }
